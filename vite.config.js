68f274b3965e64424a06cde48f6938d835c28e52
Bijay B.K <bijaybk307@gmail.com>
2025-05-14 00:25:57 +0545

    feat: configure server settings in Vite for external network access

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4000,
+    host: true // This is needed to expose the server to external networks
+  }
 })
